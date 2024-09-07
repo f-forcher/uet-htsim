d2e23a53c836a29132c6b00ce2b8ec84995ecc20
Mark Handley <mark@handley.org.uk>
2024-09-07 02:32:35 +0100

    fix formatting, improve error handling

--- htsim/sim/datacenter/check_regressions.py
+++ htsim/sim/datacenter/check_regressions.py
@@ -52,27 +52,30 @@ while True:
     if otokens[0] == "Experiment:":
         if oline != nline:
             print("ERROR: Experiment name mismatch at line", c)
-            print("  Original: ", oline)
-            print("  New:      ", nline)
+            print("  Original: ", oline, end='')
+            print("  New:      ", nline, end='')
             sys.exit()
         experiment = oline
 
     if otokens[0] == "[PASS]":
         if ntokens[0] == "[FAIL]":
+            print(experiment, end='')
             print("FAIL: test previously passes, now fails at line", c)
-            print("   ", oline)
+            print("   ", oline, end='')
             print("   ", nline)
             fail = True
         elif ntokens[0] != "[PASS]":
+            print(experiment, end='')
             print("ERROR: format mismatch at line", c, otokens[0], " vs ", ntokens[0])
-            print("   ", oline)
+            print("   ", oline, end='')
             print("   ", nline)
             sys.exit()
 
     if otokens[0] == "[FAIL]":
         if ntokens[0] == "[PASS]":
+            print(experiment, end='')
             print("NOTE: test previously failed, now passes at line", c)
-            print("   ", oline)
+            print("   ", oline, end='')
             print("   ", nline)
         elif ntokens[0] != "[FAIL]":
             print("ERROR: format mismatch at line", c, otokens[0], " vs ", ntokens[0])
@@ -81,45 +84,48 @@ while True:
     if otokens[0] == "[PASS]" or otokens[0] == "[FAIL]":
         if otokens[1] == "Connection":
             if ntokens[1] != "Connection":
+                print(experiment, end='')
                 print("ERROR: format mismatch at line", c, otokens[1], " vs ", ntokens[1])
                 sys.exit()
             ocount = int(otokens[3])
             ncount = int(ntokens[3])
             if ocount != ncount:
                 if otokens[0] == "[PASS]" and otokens[0] == "[PASS]":
-                    print(experiment)
+                    print(experiment, end='')
                     print("FAIL: mismatch in completed connections of passing test at line", c)
-                    print("   ", oline)
+                    print("   ", oline, end='')
                     print("   ", nline)
             else:
                 #print("completed connections match", ocount)
                 pass
         if otokens[1] == "Tail":
             if ntokens[1] != "Tail":
+                print(experiment, end='')
                 print("ERROR: format mismatch at line", c, otokens[1], " vs ", ntokens[1])
-                sys.exit()
+                print("   ", oline, end='')
+                print("   ", nline)
             ofct = float(otokens[3])
             nfct = float(ntokens[3])
             if nfct < ofct * 0.95:
-                print(experiment)
+                print(experiment, end='')
                 print("NOTE: tail FCT decreased significantly at line", c)
-                print("   ", oline)
+                print("   ", oline, end='')
                 print("   ", nline)
             elif nfct < ofct * 0.99:
-                print(experiment)
+                print(experiment, end='')
                 print("NOTE: tail FCT decreased slightly at line", c)
-                print("   ", oline)
+                print("   ", oline, end='')
                 print("   ", nline)
             elif ofct * 1.05 < nfct:
-                print(experiment)
+                print(experiment, end='')
                 print("FAIL: tail FCT increased significantly at line", c)
-                print("   ", oline)
+                print("   ", oline, end='')
                 print("   ", nline)
                 fail = True
             elif ofct * 1.01 < nfct:
-                print(experiment)
+                print(experiment, end='')
                 print("WARNING: tail FCT increased slightly at line", c)
-                print("   ", oline)
+                print("   ", oline, end='')
                 print("   ", nline)
                 warn = True
             elif ofct == nfct:
@@ -131,7 +137,9 @@ while True:
     if otokens[0] == "Summary:":
         if ntokens[0] != "Summary:":
             print("ERROR: format mismatch at line", c, otokens[0], " vs ", ntokens[0])
-            sys.exit()
+            print("   ", oline, end='')
+            print("   ", nline)
+            
         if otokens[1] == "New:":
             if ntokens[1] != "New:":
                 print("ERROR: format mismatch at line", c, otokens[1], " vs ", ntokens[1])
@@ -139,7 +147,9 @@ while True:
             onew = int(otokens[2])
             nnew = int(ntokens[2])
             if onew != nnew:
-                print("VALIDATION FAILURE: different number of new packets sent at line", c)
+                print("FAIL: different number of new packets sent at line", c)
+                print("   ", oline, end='')
+                print("   ", nline)
             else:
                 #print("new pkts match")
                 pass
@@ -150,18 +160,18 @@ while True:
             ortx = int(otokens[4])
             nrtx = int(ntokens[4])
             if ortx > nrtx * 1.2:
-                print(experiment)
+                print(experiment, end='')
                 print("NOTE: Significant reduction in retransmissions at line", c)
-                print("   ", oline)
+                print("   ", oline, end='')
                 print("   ", nline)
             if nrtx > ortx * 1.2:
-                print(experiment)
+                print(experiment, end='')
                 print("FAIL: Significant increase in retransmissions at line", c)
-                print("   ", oline)
+                print("   ", oline, end='')
                 print("   ", nline)
                 fail = True
             if nrtx == ortx:
-                #print("rtx matches")
+                #print("rtx matches")p
                 pass
         if otokens[9] == "ACKs:":
             if ntokens[9] != "ACKs:":
@@ -170,14 +180,14 @@ while True:
             oacks = int(otokens[10])
             nacks = int(ntokens[10])
             if oacks > nacks * 1.2:
-                print(experiment)
+                print(experiment, end='')
                 print("NOTE: Significant reduction in ACKs at line", c)
-                print("   ", oline)
+                print("   ", oline, end='')
                 print("   ", nline)
             if nacks > oacks* 1.2:
-                print(experiment)
+                print(experiment, end='')
                 print("WARNING: Significant increase in ACKs at line", c)
-                print("   ", oline)
+                print("   ", oline, end='')
                 print("   ", nline)
                 warn = True
             if nacks == oacks:
