0225931599cf5f936bdbd2d37f7a668ab1082f52
Francesco Forcher <francesco.forcher@gmail.com>
2025-09-14 15:15:09 +0200

    Removed pin on very old python packages versions, added with uv

--- flake.nix
+++ flake.nix
@@ -3,37 +3,58 @@
 
   inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1";
 
-  outputs = inputs:
+  outputs =
+    inputs:
     let
-      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
-      forEachSupportedSystem = f: inputs.nixpkgs.lib.genAttrs supportedSystems (system: f {
-        pkgs = import inputs.nixpkgs { inherit system; };
-      });
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forEachSupportedSystem =
+        f:
+        inputs.nixpkgs.lib.genAttrs supportedSystems (
+          system:
+          f {
+            pkgs = import inputs.nixpkgs { inherit system; };
+          }
+        );
     in
     {
-      devShells = forEachSupportedSystem ({ pkgs }: {
-        default = pkgs.mkShell.override
-          {
-            # Override stdenv in order to change compiler:
-	    # TODO Use 21
-            stdenv = pkgs.llvmPackages_20.libcxxStdenv;
-          }
-          {
-            packages = with pkgs; [
-              clang-tools
-              cmake
-              libcxx
-              #codespell
-              #conan
-              #cppcheck
-              #doxygen
-              #gtest
-              #lcov
-              #vcpkg
-              #vcpkg-tool
-            ] ++ (if system == "aarch64-darwin" then [ ] else [ pkgs.lldb ]);
-          };
-      });
+      devShells = forEachSupportedSystem (
+        { pkgs }:
+        {
+          default =
+            pkgs.mkShell.override
+              {
+                # Override stdenv in order to change compiler:
+                # TODO Use 21
+                stdenv = pkgs.llvmPackages_20.libcxxStdenv;
+              }
+              {
+                packages =
+                  with pkgs;
+                  [
+                    # C++
+                    clang-tools
+                    cmake
+                    libcxx
+                    #codespell
+                    #conan
+                    #cppcheck
+                    #doxygen
+                    #gtest
+                    #lcov
+                    #vcpkg
+                    #vcpkg-tool
+
+                    # Python
+                    pkgs.uv
+                  ]
+                  ++ (if system == "aarch64-darwin" then [ ] else [ pkgs.lldb ]);
+              };
+        }
+      );
     };
 }
-
